a895be44a5862d02c66ada226905468bd6bcf881
nick nderitu <30488974+nicksanchezc137@users.noreply.github.com>
2020-06-04 08:17:27 +0300

    Update Api.js

--- services/Api.js
+++ services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-  baseURL: 'https://elitesolutions.co.ke/debe/api/',
+  baseURL: '',
   //timeout: 2000,
   headers: { 'Content-Type': 'application/json' }
 });
